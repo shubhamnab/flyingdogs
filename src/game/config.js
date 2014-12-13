e203d57a67ff3ca8286f9c1e3f7398edbd6dccc0
shubham <shubham@example.com>
2014-12-14 01:34:29 +0530

    ds

---
+++ src/game/config.js
@@ -0,0 +1,22 @@
+pandaConfig = {
+    name: 'Flying Dog',
+    version: '1.0.0',
+    
+    system: {
+        width: 768,
+        height: 1024,
+        rotateScreen: true,
+        bgColor: '#000000'
+    },
+    analytics: {
+        id: 'UA-42024756-5'
+    },
+    storage: {
+        id: 'net.pandajs.flyingdog'
+    },
+    iOS8: {
+        system: {
+            webGL: true
+        }
+    }
+};
